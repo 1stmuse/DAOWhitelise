ff6280c87e9f68d8958aeca19cfa477137919035
1stmuse <olysegs@gmail.com>
2022-11-19 23:04:28 +0100

    first commit

---
+++ whitelistDAPP-contract/contracts/Whitelist.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Whitelist {
+
+    //Max number of whitelisted addresses allowed
+    uint8 public maxWhitelistedAddresses;
+
+    mapping(address => bool) public whitelistedAddresses;
+
+    uint8 public numAddressesWhitelisted;
+
+    constructor(uint8 _maxWhitelistedAddresses){
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
+    }
+
+    function addAddressToWhitelist() public {
+        //check if user has already been whitelisted
+        require(!whitelistedAddresses[msg.sender], "Sender has already been whitelisted");
+
+        // check if the number of add whitelisted is less than maxwhistedAddress
+        require(numAddressesWhitelisted < maxWhitelistedAddresses, "More addresses cant be added, limit reached");
+
+        whitelistedAddresses[msg.sender] = true;
+
+        numAddressesWhitelisted += 1;
+    }
+}
